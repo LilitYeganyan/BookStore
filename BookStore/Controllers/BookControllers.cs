fb9efddc275bb8aa69fec6de8ecf5c59a3478f82
lilit.eganyanel53@gmail.com <lilit.eganyanel53@gmail.com>
2022-03-31 15:08:52 +0400

    Add project files.

---
+++ BookStore/Controllers/BookControllers.cs
@@ -0,0 +1,50 @@
+﻿using BookStroe.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class BookControllers : ControllerBase
+    {
+        private readonly IBookServices _bookServices;
+        public BookControllers(IBookServices bookServices)
+        {
+            this._bookServices = bookServices;
+        }
+
+        [HttpGet]
+        public IActionResult GetBooks()
+        {
+            return Ok(_bookServices.GetBooks());
+        }
+
+        [HttpGet("{id}", Name = "GetBook")]
+        public IActionResult GetBook(string id)
+        {
+            return Ok(_bookServices.GetBook(id));
+        }
+
+        [HttpPost]
+        public IActionResult AddBook(Book book)
+        {
+            _bookServices.AddBook(book);
+            return CreatedAtRoute("GetBook", new { id = book.Id }, book);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBook(string id)
+        {
+            _bookServices.DeleteBook(id);
+            return NoContent();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateBook(Book book)
+        {
+            return Ok(_bookServices.UpdateBook(book));
+        }
+       
+
+    }
+}
---
+++ BookStroe.Core/BookStoreDbConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStroe.Core
+{
+    public class BookStoreDbConfig
+    {
+        public string Databace_Name { get; set; }
+        public string Books_Collection_Name { get; set; }
+        public string Connection_String { get; set; }
+    }
+}
